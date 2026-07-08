613115f2d73ecf5cab8d61edd74633670f8ec14c
SuperiorInNoctem <sin.vidar@gmail.com>
2026-07-08 15:38:54 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 module.exports = function(eleventyConfig) {
-// Kopieer CSS, afbeeldingen en de CMS admin map rechtstreeks naar de website
-eleventyConfig.addPassthroughCopy("assets");
-eleventyConfig.addPassthroughCopy("admin");
-eleventyConfig.addPassthroughCopy("data");
-return {
-dir: {
-input: ".",
-output: "_site",
-includes: "_includes",
-data: "_data"
-}
-};
+    // Kopieer alleen de assets en admin mappen naar de live website
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("admin");
+
+    return {
+        dir: {
+            input: ".",
+            output: "_site",
+            includes: "_includes",
+            data: "_data"
+        }
+    };
 };
